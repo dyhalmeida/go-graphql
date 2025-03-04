1cd57ed90cf28f7b07a9d7078a7404bdbeeaf8d1
dyhalmeida <dyhalmeida@gmail.com>
2025-03-04 11:17:59 -0300

    feat: add courses table with foreign key reference to categories
    docs: to execute the script table in a bank called database in sqlite, i used the command: sqlite3 database.db < ./internal/database/create_table.sql

--- internal/database/create_table.sql
+++ internal/database/create_table.sql
@@ -3,3 +3,11 @@ CREATE TABLE IF NOT EXISTS categories (
     name TEXT NOT NULL,
     description TEXT
 );
+
+CREATE TABLE IF NOT EXISTS courses (
+    id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT,
+    categoryId TEXT NOT NULL,
+    FOREIGN KEY (categoryId) REFERENCES categories(id) ON DELETE CASCADE
+)
